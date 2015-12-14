9252abc7eb00a47a11711914e32f1664510b8a7c
Thomas Dufour <thomas.dufour@epitech.eu>
2015-12-14 17:20:36 +0100

    [blinux-make] Expect test defs in 'mouli' sub-folder

--- blinux-make/make_and_test.sh
+++ blinux-make/make_and_test.sh
@@ -1,8 +1,34 @@
 #!/bin/sh -x
 
-~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note
-[ -f Makefile ] && make
+IPWD=$PWD
+
+# check that the manifest & associated files are in $PWD/mouli
+if [[ ! -f mouli/manifest.json ]]
+then
+    echo "This version of bugs requires this project to be reconfigured, please run the matching SEED job."
+    exit 1
+fi
+
+# Norm check
+(cd mouli && ~/norme_deepthought.py rendu -score -nocheat -swap_traces -malloc >> ~/workspace/norm.dpr 2> ~/workspace/norm.note)
+
+# make (1/2 - mouli)
+[ -f mouli/Makefile ] && make -C mouli
+
+# make (2/2 - student)
 [ -f rendu/Makefile ] && make -C rendu $MAKE_RULE
-cp -rn rendu/* .
+cp -rn rendu/* mouli
+
+# run bugs
+cd mouli
 bugs -version
 bugs -out "graph txt xunit" -login $DBUSER -city $CITY
+
+# retrieve run artifacts
+cd $IPWD
+cp mouli/trace.txt .
+cp mouli/note.txt .
+cp mouli/tests_report.xml .
+
+# wipe mouli folder
+rm -rf mouli
